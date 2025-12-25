e07fcfb89c498328178705a9adc0c31a2b57c151
Paulus sesa <114337187+seanpaulsesa@users.noreply.github.com>
2025-12-25 00:12:31 -0800

    added file project pascal

---
+++ programmstrukturberurut.pas
@@ -0,0 +1,14 @@
+program StrukturBerurut;
+var
+  nama: string;
+  umur: integer;
+begin
+  writeln('Masukkan nama: ');
+  readln(nama);
+
+  writeln('Masukkan umur: ');
+  readln(umur);
+
+  writeln('Nama Anda: ', nama);
+  writeln('Umur Anda: ', umur);
+end.
